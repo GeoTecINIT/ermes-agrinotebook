ce8980512f94ec3227ae07c9758572bac1b52919
albertogonper <alberto.gonzalez95@gmail.com>
2015-11-21 14:33:05 +0100

    Soil condition view linked with model

--- app/controllers/index/soil-condition.js
+++ app/controllers/index/soil-condition.js
@@ -1,15 +1,11 @@
 import Ember from 'ember';
 import * as dd from 'ermes-smart-app/models/static/soil-condition';
+import ProductUpload from 'ermes-smart-app/mixins/product-upload';
 
-export default Ember.Controller.extend({
+export default Ember.Controller.extend(ProductUpload, {
   panelId: 'soil-condition',
   i18n: Ember.inject.service(),
   parcelStatus: Ember.computed('i18n.locale', function () {
     return dd.getParcelStatus(this);
-  }),
-  actions: {
-    submit() {
-
-    }
-  }
+  })
 });
