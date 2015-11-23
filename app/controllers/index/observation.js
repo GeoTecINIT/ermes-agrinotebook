82fb26ea567ce337cf1eafd6bc2363c83f7ad804
albertogonper <alberto.gonzalez95@gmail.com>
2015-11-23 21:04:27 +0100

    Observation view linked to model

--- app/controllers/index/observation.js
+++ app/controllers/index/observation.js
@@ -1,10 +1,17 @@
 import Ember from 'ember';
+import ProductUpload from 'ermes-smart-app/mixins/product-upload';
 
-export default Ember.Controller.extend({
+export default Ember.Controller.extend(ProductUpload, {
   panelId: 'observation',
   actions: {
     submit() {
-
+      if (!this.get('files')) {
+        this.set('fileError', 'Please upload an image');
+      } else {
+        this.set('fileError', '');
+        // Manage file upload
+        this._super(this);
+      }
     }
   }
 });
