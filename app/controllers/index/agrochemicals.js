1e6dd6aad0fb683c8f6c9a7695eba62dfcf5f31c
albertogonper <alberto.gonzalez95@gmail.com>
2015-11-23 20:45:16 +0100

    Agrochemicals view wired up with the model

--- app/controllers/index/agrochemicals.js
+++ app/controllers/index/agrochemicals.js
@@ -1,15 +1,11 @@
 import Ember from 'ember';
 import * as dd from 'ermes-smart-app/models/static/agrochemicals';
+import ProductUploadRDate from 'ermes-smart-app/mixins/product-upload-rdate';
 
-export default Ember.Controller.extend({
+export default Ember.Controller.extend( ProductUploadRDate, {
   panelId: 'agrochemicals',
   i18n: Ember.inject.service(),
   products: Ember.computed('i18n.locale', function () {
     return dd.getProducts(this);
-  }),
-  actions: {
-    submit() {
-
-    }
-  }
+  })
 });
