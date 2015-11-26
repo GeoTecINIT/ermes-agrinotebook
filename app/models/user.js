5468e5f8b8f4bf3e5b80568542f39cfd6118c253
albertogonper <alberto.gonzalez95@gmail.com>
2015-11-26 23:22:41 +0100

    User model loaded

--- app/models/user.js
+++ app/models/user.js
@@ -1,16 +1,17 @@
 import DS from 'ember-data';
 
 export default DS.Model.extend({
+  //_id: DS.attr('string'),
   username: DS.attr('string'),
   email: DS.attr('string'),
   region: DS.attr('string'),
   profile: DS.attr('string'),
-  language: DS.attr('string'),
-  lastPosition: {
-    spatialReference: DS.attr('number'),
-    lastX: DS.attr('number'),
-    lastY: DS.attr('number'),
-    zoom: DS.attr('number')
-  },
-  parcels: DS.hasMany('parcel')
+  language: DS.attr('string')
+  //lastPosition: {
+  //  spatialReference: DS.attr('number'),
+  //  lastX: DS.attr('number'),
+  //  lastY: DS.attr('number'),
+  //  zoom: DS.attr('number')
+  //}
+  //parcels: [DS.attr('string')]
 });
