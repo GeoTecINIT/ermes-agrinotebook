3062f23057189a64f2f315a3de83753964ff41b8
albertogonper <alberto.gonzalez95@gmail.com>
2015-11-29 01:04:26 +0100

    Panel reset on product succesfully uploaded

--- app/mixins/product-upload.js
+++ app/mixins/product-upload.js
@@ -2,10 +2,8 @@ import Ember from 'ember';
 import Moment from 'moment';
 import config from '../config/environment';
 
-var dFormat = config.APP.defaultDateFormat;
-
 export default Ember.Mixin.create({
-  parcels: Ember.inject.service(),
+  productService: Ember.inject.service('products'),
   actions: {
     submit() {
       if (this.get('parcels.selectedParcels').length <= 0) {
@@ -14,10 +12,13 @@ export default Ember.Mixin.create({
         this.set('parcelError', '');
         this.set('model.parcels', this.get('parcels.selectedParcels'));
         this.set('model.uploadingDate', new Moment());
-        this.get('model').save().then(function () {
-          console.debug('OK!!');
-        }, function () {
-          console.debug(';(');
+        console.debug('uploading product...');
+        this.get('model').save().then(() => {
+          this.set('model', this.get('productService')
+            .archiveProduct(Ember.String.singularize(this.get('panelId'))));
+          console.debug('success!')
+        }, () => {
+          console.debug('ERROR');
         });
       }
     }
--- app/services/products.js
+++ app/services/products.js
@@ -8,6 +8,12 @@ export default Ember.Service.extend({
     this.set('products', products);
   },
 
+  /**
+   * Product singleton factory, retrieves an existent product or
+   * instances a new one if there's not
+   * @param productName
+   * @returns product
+     */
   getProduct(productName) {
     if (this.get('products.'+productName)) {
       return this.get('products.'+productName);
@@ -16,5 +22,16 @@ export default Ember.Service.extend({
       this.set('products.'+productName, product);
       return product;
     }
+  },
+
+  /**
+   * Archives actual product instancing a new one
+   * @param productName
+   * @returns product
+     */
+  archiveProduct(productName) {
+    var product = this.get('store').createRecord(productName);
+    this.set('products.'+productName, product);
+    return product;
   }
 });
