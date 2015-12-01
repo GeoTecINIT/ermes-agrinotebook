545197239fc2ab449e988ad13fbd591446f17422
albertogonper <alberto.gonzalez95@gmail.com>
2015-12-01 21:57:43 +0100

    Greek translation revised

--- app/models/static/regions.js
+++ app/models/static/regions.js
@@ -11,6 +11,6 @@ export function getLanguajes() {
     {text: 'English', value: 'en', selected: true},
     {text: 'Español', value: 'es'},
     {text: 'Italiano', value: 'it'},
-    {text: 'Έλληνες', value: 'gk'}
+    {text: 'Ελληνικά', value: 'gk'}
   ];
 }
