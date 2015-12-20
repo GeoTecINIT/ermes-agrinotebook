3e7816961bb32c18dd57725fd58c2e320aa4073f
albertogonper <alberto.gonzalez95@gmail.com>
2015-12-20 19:21:15 +0100

    Product model factory name refactor

--- app/routes/index/weeds.js
+++ app/routes/index/weeds.js
@@ -1,7 +1,7 @@
 import Ember from 'ember';
 import AuthChecker from 'ermes-smart-app/mixins/auth-checker';
 import PanelManager from 'ermes-smart-app/mixins/panel-manager';
-import ProductModel from 'ermes-smart-app/mixins/product-model';
+import ProductModelFactory from 'ermes-smart-app/mixins/product-model-factory';
 
-export default Ember.Route.extend(AuthChecker, PanelManager, ProductModel, {
+export default Ember.Route.extend(AuthChecker, PanelManager, ProductModelFactory, {
 });
