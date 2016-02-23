26607dba670824c8d19e69be6640af1b01aea433
Alberto Gonzalez <al286290@uji.es>
2016-02-24 00:48:18 +0100

    Fixing collaborator permissions

--- app/mixins/initial-tasks.js
+++ app/mixins/initial-tasks.js
@@ -17,8 +17,9 @@ export default Ember.Mixin.create({
 
 
     user.get('parcels').then((parcels) => {
+      this.controllerFor('index').set('editMode', false);
       if (parcels.length === 0) {
-        if (navigator.onLine) {
+        if (navigator.onLine && user.get('type') !== 'collaborator') {
           this.controllerFor('index').set('editMode', true);
           this.transitionTo('index.welcome');
         } else {
