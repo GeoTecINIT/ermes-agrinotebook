e15f11510845128cb78dc999d19714bbff69cb8e
Alberto Gonzalez <al286290@uji.es>
2016-02-24 20:39:27 +0100

    Proxy under revision

--- config/environment.js
+++ config/environment.js
@@ -15,7 +15,7 @@ module.exports = function(environment) {
 
     APP: {
       apiServer: "http://ermes.dlsi.uji.es:6787",
-      layerProxy: "http://ermes.dlsi.uji.es:6797/proxy",
+      layerProxy: "http://ermes.dlsi.uji.es:6686/proxy",
 
       //resourcesServer: "http://altairi.lan:4200/",
       resourcesServer: "http://geo4.dlsi.uji.es:8080/",
@@ -107,7 +107,7 @@ module.exports = function(environment) {
     'default-src': "'none'",
     'script-src': "'self' 'unsafe-eval' 'unsafe-inline' :49152 http://ermes.dlsi.uji.es:6080/arcgis/rest/services/ http://js.arcgis.com/ https://js.arcgis.com/",
     'font-src': "'self'",
-    'connect-src': "'self' http://geo4.dlsi.uji.es:8080/ http://ermes.dlsi.uji.es:6080/arcgis/rest/ http://ermes.dlsi.uji.es:6787/ http://server.arcgisonline.com/arcgis/rest/services/ http://services.arcgisonline.com/ http://www.arcgis.com/ http://static.arcgis.com/",
+    'connect-src': "'self' http://geo4.dlsi.uji.es:8080/ http://ermes.dlsi.uji.es:6080/arcgis/rest/ http://ermes.dlsi.uji.es:6787/ http://ermes.dlsi.uji.es:6787/ http://server.arcgisonline.com/arcgis/rest/services/ http://services.arcgisonline.com/ http://www.arcgis.com/ http://static.arcgis.com/",
     'img-src': "'self' http://ermes.dlsi.uji.es http://ermes.dlsi.uji.es:6787/ http://ermes.dlsi.uji.es:6080/arcgis/rest/services/ void: data: http://js.arcgis.com/ http://services.arcgisonline.com/ http://server.arcgisonline.com/",
     'style-src': "'self' 'unsafe-inline'",
     'media-src': "'self'"
